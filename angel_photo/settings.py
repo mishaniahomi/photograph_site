e890a937da1da8c78aa6dc008fad55a613e87d71
mishaniahomi <moneti.misha@yandex.ru>
2023-02-06 09:52:08 +0000

    На гитхаб

--- angel_photo/settings.py
+++ angel_photo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_d8%2e#16oiaks1vkb#)rabf03@b25hwn&t31ev4s+^-&hj_*-'
+SECRET_KEY = 'your_secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
